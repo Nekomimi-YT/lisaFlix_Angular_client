dc7ece655de99139cb19dcfcfc6981d65e12f491
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-11-29 18:40:09 +0100

    feat: Add method for accessing login form component in login dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -17,6 +18,14 @@ export class AppComponent {
   // This is the function that will open the dialog when the signup button is clicked  
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
+    // Assigning the dialog a width
+    width: '280px'
+    });
+  }
+
+  // This is the function that will open the dialog when the login button is clicked  
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
     // Assigning the dialog a width
     width: '280px'
     });
