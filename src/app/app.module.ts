8f024386c6c4d49ab587f4a78fe859738bead5ed
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-11-29 12:51:33 +0100

    feat: Add UserRegistrationFormComponent to importa and declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationFormComponent
   ],
   imports: [
     BrowserModule,
