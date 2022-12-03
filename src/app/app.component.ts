887554b18c3110304585a4a1b401d69734c91128
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-12-03 13:42:20 +0100

    feat: Import MovieCardComponent and logic for opening the all movies dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -28,6 +29,13 @@ export class AppComponent {
     this.dialog.open(UserLoginFormComponent, {
     // Assigning the dialog a width
     width: '280px'
+    });
+  }
+
+  // This is the function that will open the dialog when the get all movies button is clicked  
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
     });
   }
 }
