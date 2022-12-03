1f915761c45c66cb9a57066148dbf29cf867b8ad
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-12-03 13:43:12 +0100

    fix: update component name

--- src/app/fetch-api-data.service.spec.ts
+++ src/app/fetch-api-data.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { FetchApiDataService } from './fetch-api-data.service';
+import { fetchApiDataService } from './fetch-api-data.service';
 
 describe('FetchApiDataService', () => {
-  let service: FetchApiDataService;
+  let service: fetchApiDataService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(FetchApiDataService);
+    service = TestBed.inject(fetchApiDataService);
   });
 
   it('should be created', () => {
