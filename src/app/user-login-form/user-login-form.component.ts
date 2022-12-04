0b4a655499de433580ebf2d52c58010f10d7e366
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-12-04 09:50:45 +0100

    feat: Set username and token to local storage

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -32,8 +32,10 @@ export class UserLoginFormComponent {
   loginUser(): void {
     this.fetchApiData.userLogin(this.userData).subscribe((result) => {
     // Logic for a successful user login goes here! (To be implemented)
-    this.dialogRef.close(); // This will close the modal on success!
     console.log(result)
+    localStorage.setItem('user', result.user.Username);
+    localStorage.setItem('token', result.token);
+    this.dialogRef.close(); // This will close the modal on success!
     this.snackBar.open(result, 'OK', {
         duration: 3000
     });
