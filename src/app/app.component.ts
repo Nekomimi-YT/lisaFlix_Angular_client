da649d344f655916563d950511652e7a3114c434
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-12-05 09:27:53 +0100

    refactor: Remove welcome page dialogs and related imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
-import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -12,30 +8,5 @@ import { MatDialog } from '@angular/material/dialog';
 })
 
 export class AppComponent {
-  title = 'lisFlix-Angular-client';
-
-  constructor(public dialog: MatDialog) { }
-  
-  // This is the function that will open the dialog when the signup button is clicked  
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-    // Assigning the dialog a width
-    width: '280px'
-    });
-  }
-
-  // This is the function that will open the dialog when the login button is clicked  
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-    // Assigning the dialog a width
-    width: '280px'
-    });
-  }
-
-  // This is the function that will open the dialog when the get all movies button is clicked  
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: '500px'
-    });
-  }
+  title = 'lisFlix-Angular-client'
 }
