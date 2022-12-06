4e9dbdfbe92accdb4841c986baaa43caad2c24f8
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-12-06 08:25:30 +0100

    chore: clean up code

--- src/app/director/director.component.ts
+++ src/app/director/director.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Inject} from '@angular/core';
-
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -7,7 +6,8 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   templateUrl: './director.component.html',
   styleUrls: ['./director.component.scss']
 })
-export class DirectorComponent implements OnInit{
+
+export class DirectorComponent implements OnInit {
   constructor(
     @Inject(MAT_DIALOG_DATA)
     public data: {
