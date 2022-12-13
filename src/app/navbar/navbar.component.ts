98c5e43012c216616264ed599fc80ba3d2aa728f
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-12-13 10:11:22 +0100

    fix: remove favorites option and update styling

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-navbar',
@@ -11,11 +12,24 @@ export class NavbarComponent {
   user = localStorage.getItem('user');
 
   constructor(
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,
     private router: Router
   ) { }
 
+  // Route to profile page
   openProfileView(): void {
     this.router.navigate(['profile']);
   }
 
+  // Logout send user back to welcome page
+  logoutUser(): void {
+    localStorage.clear();
+    this.router.navigate(['welcome'])
+    .then(() => {
+      this.snackBar.open('You have successfully logged out', 'OK', {
+        duration: 3500
+      });
+    });
+  }
 }
