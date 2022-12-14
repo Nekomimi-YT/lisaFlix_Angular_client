dca6dd12cfa7ba2aac93778936e2a363866691af
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-12-14 11:14:04 +0100

    fix: Remove result and add correct user confirmation to snackbar. chore: update comments

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,13 +1,13 @@
 // src/app/user-registration-form/user-registration-form.component.ts
 import { Component, OnInit, Input } from '@angular/core';
 
-// You'll use this import to close the dialog on success
+// Closes dialogs
 import { MatDialogRef } from '@angular/material/dialog';
 
-// This import brings in the API calls we created in 6.2
+// Connects to the API call service
 import { FetchApiDataService } from '../fetch-api-data.service';
 
-// This import is used to display notifications back to the user
+// Display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
@@ -28,16 +28,16 @@ export class UserRegistrationFormComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  // This is the function responsible for sending the form inputs to the backend
+  // Send the form inputs to the backend
   registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
-    this.dialogRef.close(); // This will close the modal on success!
+    this.dialogRef.close(); // Close the modal on success
     console.log(result)
-    this.snackBar.open(result, 'OK', {
+    this.snackBar.open("Success! Please login.", 'OK', {
         duration: 4000
     });
     }, (result) => {
-      this.snackBar.open(result, 'OK', {
+      this.snackBar.open("Oops! Try again!", 'OK', {
         duration: 4000
       });
     });
